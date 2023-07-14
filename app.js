a5192dc32259bb8475e1c4058d2a9d9414915713
hangju0610 <phj950610@naver.com>
2023-07-15 03:39:38 +0900

    CICD test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const a = require('./');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.send('씨발');
 });
 
 app.listen(3000, (req, res) => {
